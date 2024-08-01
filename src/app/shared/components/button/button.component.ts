0f3b739e2fd824403977fcf8b3380cf95c81600b
akkitheakhil <speaktoakhilp@gmail.com>
2024-08-01 10:42:01 +0200

    feat: Remove example code

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,10 +1,4 @@
-import {
-  booleanAttribute,
-  Component,
-  input,
-  output,
-  signal,
-} from '@angular/core';
+import { booleanAttribute, Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -21,10 +15,8 @@ export class ButtonComponent {
     transform: booleanAttribute,
   });
   onBtnClick = output<MouseEvent>();
-  count = signal<number>(0);
 
   handleClick(event: MouseEvent): void {
     this.onBtnClick.emit(event);
-    this.count.update((count) => count + 1);
   }
 }
